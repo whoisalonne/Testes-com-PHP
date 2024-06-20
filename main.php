30f640d795fc4c0bfeff573dfc588e0eba4e2eaf
Janderson Lima <backupaw197@gmail.com>
2024-06-20 20:33:15 -0300

    TESTE 1

--- main.php
+++ main.php
@@ -1,9 +1,8 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $nome = htmlspecialchars($_POST['nome']);
     $email = htmlspecialchars($_POST['email']);
     echo "<h2>Dados recebidos via POST:</h2>";
     echo "Nome: " . $nome . "<br>";
     echo "Email: " . $email . "<br>";
-}
